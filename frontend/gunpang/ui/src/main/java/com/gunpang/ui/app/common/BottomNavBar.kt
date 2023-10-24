2c401e7c0fc6765b2c22c21fb4fbfb5f0bbf93a6
권기윤 <ceasar0830@gmail.com>
2023-10-25 00:37:42 +0900

    fix : design system

--- frontend/gunpang/ui/src/main/java/com/gunpang/ui/app/common/BottomNavBar.kt
+++ frontend/gunpang/ui/src/main/java/com/gunpang/ui/app/common/BottomNavBar.kt
@@ -2,8 +2,17 @@ package com.gunpang.ui.app.common
 
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 fun BottomNavBar() {
-    //BottomAppBar()
+//    BottomAppBar(
+//        modifier= Modifier,
+//        containerColor= Color,
+//        contentColor= Color,
+//        tonalElevation= Dp,
+//        contentPadding= PaddingValues,
+//        windowInsets= WindowInsets,
+//        content= @Composable() (RowScope.() -> Unit
+//    )
 }
