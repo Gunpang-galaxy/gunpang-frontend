cd624f0b34cd4b9069ee9014b120312e856076b7
박은정 <dms011221@gmail.com>
2023-10-26 15:40:04 +0900

    Page Indicator변경

--- frontend/gunpang/watch-ui/src/main/java/com/gunpang/watch_ui/common/GunpangScreenWrapper.kt
+++ frontend/gunpang/watch-ui/src/main/java/com/gunpang/watch_ui/common/GunpangScreenWrapper.kt
@@ -5,9 +5,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.gunpang.watch_ui.theme.Gray900
 import com.gunpang.watch_ui.theme.GunpangTheme
 
@@ -16,10 +18,12 @@ fun GunpangScreenWrapper(
     content: @Composable () -> Unit
 ) {
     GunpangTheme {
+
         Box(
             modifier = Modifier
                 .fillMaxSize()
-                .background(Gray900),
+                .background(Gray900)
+                .padding(15.dp),
             contentAlignment = Alignment.Center,
         ) {
             Column(
--- frontend/gunpang/watch-ui/src/main/java/com/gunpang/watch_ui/exercise/OnExerciseScreen.kt
+++ frontend/gunpang/watch-ui/src/main/java/com/gunpang/watch_ui/exercise/OnExerciseScreen.kt
@@ -1,7 +1,10 @@
 package com.gunpang.watch_ui.exercise
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -22,40 +25,46 @@ import com.gunpang.common.R
 @Composable
 fun OnExerciseScreen() {
     GunpangScreenWrapper {
-        Image(
-            painter = painterResource(id = R.drawable.baccus), // 여기서 'your_icon'을 실제 리소스 이름으로 바꾸어야 합니다.
-            contentDescription = "아령이미지",
-            modifier = Modifier.size(36.dp)
-        )
-        TimeShow(minute="33",second="10")
-        WatchDivider(width=100.dp)
-        BpmShow(bpm="180")
-        Image(
-            painter = painterResource(id = R.drawable.baccus), // 여기서 'your_icon'을 실제 리소스 이름으로 바꾸어야 합니다.
-            contentDescription = "하트이미지",
-            modifier = Modifier.size(36.dp)
-        )
+        Column(modifier = Modifier.fillMaxSize() ,horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(
+                painter = painterResource(id = R.drawable.dumbell),
+                contentDescription = "아령이미지",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            )
+            TimeShow(minute="33",second="10")
+            WatchDivider(fraction=0.8f, thickness = 5.dp)
+            BpmShow(bpm="180")
+            Image(
+                painter = painterResource(id = R.drawable.heart),
+                contentDescription = "하트이미지",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+            )
+        }
     }
 }
 @Composable
 fun TimeShow(minute:String="10",second:String="10"){
     Row(
-        modifier = Modifier.padding(8.dp),
+        modifier = Modifier.padding(bottom = 4.dp),
         verticalAlignment = Alignment.Bottom
     ) {
-        Text(text = minute, fontSize = 25.sp)
-        Text(text = "m",Modifier.paddingFromBaseline(bottom=6.dp))
-        Text(text = second, fontSize = 25.sp)
-        Text(text = "s",Modifier.paddingFromBaseline(bottom=6.dp))
+        Text(text = minute, fontSize = 25.sp, modifier = Modifier.padding(1.dp))
+        Text(text = "m",Modifier.paddingFromBaseline(bottom=3.dp).padding(1.dp))
+        Text(text = second, fontSize = 25.sp, modifier = Modifier.padding(1.dp))
+        Text(text = "s",Modifier.paddingFromBaseline(bottom=3.dp).padding(1.dp))
     }
 }
 @Composable
 fun BpmShow(bpm:String="128"){
     Row(
-        modifier = Modifier.padding(8.dp),
+        modifier = Modifier.padding(top= 2.dp),
         verticalAlignment = Alignment.Bottom
     ) {
         Text(text = bpm, fontSize = 25.sp)
-        Text(text = "bpm",Modifier.paddingFromBaseline(bottom=6.dp))
+        Text(text = "bpm",Modifier.paddingFromBaseline(bottom=3.dp).padding(1.dp))
     }
 }
