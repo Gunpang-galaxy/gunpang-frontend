c01e10b0fce2e5f55c9f719ffb82b0f6c5c74c53
권기윤 <ceasar0830@gmail.com>
2023-10-31 16:48:01 +0900

    fix : AvatarCode, MealRecordCode 수정

--- frontend/gunpang/common/src/main/java/com/gunpang/common/code/AvatarCode.kt
+++ frontend/gunpang/common/src/main/java/com/gunpang/common/code/AvatarCode.kt
@@ -3,10 +3,9 @@ package com.gunpang.common.code
 import com.gunpang.common.R
 
 enum class AvatarCode(
-    val avatarTypeId: Int,
     val avatarDefaultName: String,
     val imageId: Int
 ) {
-    AVATAR_CAT(avatarTypeId = 0, avatarDefaultName = "냥퍙이" ,imageId = R.drawable.avatar_cat),
-    AVATAR_CHICK(avatarTypeId = 1, avatarDefaultName="짹팡이" ,imageId = R.drawable.avatar_chick),
+    AVATAR_CAT(avatarDefaultName = "냥퍙이" ,imageId = R.drawable.avatar_cat),
+    AVATAR_CHICK(avatarDefaultName="짹팡이" ,imageId = R.drawable.avatar_chick),
 }
--- frontend/gunpang/common/src/main/java/com/gunpang/common/code/MealRecordCode.kt
+++ frontend/gunpang/common/src/main/java/com/gunpang/common/code/MealRecordCode.kt
@@ -3,10 +3,11 @@ package com.gunpang.common.code
 import com.gunpang.common.R
 
 enum class MealRecordCode (
-    val image: Int
+    val foodType: String,
+    val imageId: Int
 ){
-    HEALTHY(R.drawable.food_healthy_salad),
-    NORMAL(R.drawable.food_normal_rice),
-    BAD(R.drawable.food_bad_fried),
-    NOT_RECORD(R.drawable.nothing)
+    HEALTHY(foodType="건강한 음식", imageId = R.drawable.food_healthy_salad),
+    NORMAL(foodType="적당한 음식", imageId = R.drawable.food_normal_rice),
+    BAD(foodType="나쁜 음식", imageId = R.drawable.food_bad_fried),
+    NOT_RECORD(foodType="음식 기록 안함", imageId = R.drawable.nothing)
 }
