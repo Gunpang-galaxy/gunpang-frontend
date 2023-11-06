547b7d73993bf1857d7b8fef86e704762feda59b
권기윤 <ceasar0830@gmail.com>
2023-11-06 14:03:27 +0900

    마이페이지 스크린 추가 (logout logic 필요)

--- frontend/gunpang/app-ui/src/main/java/com/gunpang/ui/app/screen/etc/DevelopScreen.kt
+++ frontend/gunpang/app-ui/src/main/java/com/gunpang/ui/app/screen/etc/DevelopScreen.kt
@@ -1,2 +1,34 @@
 package com.gunpang.ui.app.screen.etc
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import com.gunpang.ui.app.common.ContentsNoRecord
+import com.gunpang.ui.app.common.TopBar
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DevelopScreen(
+    navController: NavController,
+) {
+    Scaffold(
+        topBar={
+            TopBar(
+                navController = navController,
+                hasUndo = true
+            ) },
+    ) {
+            it -> Surface(
+        modifier = Modifier
+            .padding(it)
+    ) {
+        ContentsNoRecord(
+            reason = "추후 개발"
+        )
+    }
+    }
+}
