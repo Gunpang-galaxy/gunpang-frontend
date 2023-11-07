255afa2cda49ba5a7ab6d98dc753a95edbe33562
Yehan Park <parkyehan2060@gmail.com>
2023-11-07 18:01:19 +0900

    fix: init code COMPOSITION_NOT_SET 추가

--- frontend/gunpang/common/src/main/java/com/gunpang/common/code/InitCode.kt
+++ frontend/gunpang/common/src/main/java/com/gunpang/common/code/InitCode.kt
@@ -7,6 +7,7 @@ enum class InitCode(val description:String) {
     REGISTER("회원가입 필요"),
     NO_GOAL_SET("목표 설정 안함"),
     AVATAR_NOT_CREATED("아바타 생성 안되어 있음"),
+    COMPOSITION_NOT_SET("체지방 측정 하지 않음"),
     NOT_CONFIG("그외 설정 안됨"),
     FINISH("모든 설정 완료")
 }
