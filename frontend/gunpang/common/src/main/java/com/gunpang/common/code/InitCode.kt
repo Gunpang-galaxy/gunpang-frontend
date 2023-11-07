90d617c6fc0ba9ce6d501095f1df93b21dff1544
권기윤 <ceasar0830@gmail.com>
2023-11-07 17:49:16 +0900

    fix : init code NOT_CONFIG 추가

--- frontend/gunpang/common/src/main/java/com/gunpang/common/code/InitCode.kt
+++ frontend/gunpang/common/src/main/java/com/gunpang/common/code/InitCode.kt
@@ -5,6 +5,8 @@ enum class InitCode(val description:String) {
     NOT_INSTALL("앱 설치 안됨"),
     NOT_LOGIN("로그인 필요"),
     REGISTER("회원가입 필요"),
+    NO_GOAL_SET("목표 설정 안함"),
+    AVATAR_NOT_CREATED("아바타 생성 안되어 있음"),
     NOT_CONFIG("그외 설정 안됨"),
     FINISH("모든 설정 완료")
 }
